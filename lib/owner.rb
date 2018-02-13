4dac9c8fd68263b5a4143adf40f43252f5a2608f
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-13 15:24:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,12 +2,14 @@ class Owner
   attr_accessor :pets, :name
   attr_reader  :species
 
+  @pets = Hash.new
   @@all = Array.new
 
   def initialize(name)
     @name = name
     @@all << self
     @species = name
+    @pets = {:pets => [], :dogs => [], :cats => []}
   end
 
   def self.all
